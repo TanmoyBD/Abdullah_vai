bc16d1cd80e6f81bb4572ecadcf332dcda91b1e2
TanmoyBD <tanmoymondal1315@gmail.com>
2023-11-05 13:45:31 +0600

    Post task done

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,7 +4,11 @@ from .views import *
 
 urlpatterns = [
     path('',Home, name='home'),
+    path('create/task/',Create_Task, name='add_task'),
+    
+    
     path('api/user_tasks/', get_user_tasks, name='get_user_tasks'),
     path('api/delete_task/<int:task_id>/', delete_task, name='delete_task'),
+    path('api/create_task/', create_task_api, name='create_task'),
     
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -11,6 +11,10 @@ def Home(request):
     return render(request, 'home.html')
 
 
+def Create_Task(request):
+    return render(request, 'create_task.html')
+
+
 #JSON FORMAT_____________________________________________
 # def get_user_tasks(request, user_id):
 #     tasks = Task.objects.filter(user_id=user_id)
@@ -22,7 +26,7 @@ def Home(request):
 def get_user_tasks(request):
     if request.method == 'GET':
         user = request.user  # Assuming you're using authentication
-        tasks = Task.objects.filter(user=user, completed=False).order_by('priority_choice')
+        tasks = Task.objects.filter(user=user, completed=False).order_by('-priority_choice')
         serializer = TaskSerializer(tasks, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
     
@@ -31,3 +35,22 @@ def delete_task(request, task_id):
     task = get_object_or_404(Task, id=task_id)
     task.delete()
     return JsonResponse({'message': 'Task deleted successfully.'})
+
+    
+@api_view(['POST'])
+def create_task_api(request):
+    print("Dhukce")
+    if request.method == 'POST':
+        print(request.data)
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            print(serializer.validated_data) 
+            serializer.save(user=request.user)
+            return JsonResponse({'message': 'Task created successfully.'})
+        else:
+            print(serializer.errors)  # Print any validation errors
+            return JsonResponse(serializer.errors, status=400)
+    else:
+        return JsonResponse({'message': 'Invalid request method.'}, status=405)
+    
+    
